88ca03608b40fc238be8a0fa4e3258798d542aed
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-06-06 12:16:31 -0300

    my app file updated

---
+++ weather_app/lib/my_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Weather App',
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
