125c4370d2828f5872496358a5e997acfea8a5f1
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-06-06 13:54:20 -0300

    modular's setup done

---
+++ lib/app_module.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  
+  // @override
+  // List<Bind> get => binds [];
+
+  //  @override
+  // List<ModularRoute> get routes => [
+  //       ModuleRoute(
+  //         Modular.initialRoute,
+  //         module: LoginModule(),
+  //       ),
+  //       ModuleRoute(
+  //         '/register/',
+  //         module: RegisterModule(),
+  //       ),
+  //       ModuleRoute(
+  //         '/onboarding/',
+  //         module: OnboardingModule(),
+  //       ),
+  //     ];
+
+}
