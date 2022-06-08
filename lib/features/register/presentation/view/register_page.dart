e628eb9b94362663cce5a96b182e027f2207fb18
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-06-08 10:53:24 -0300

    WIP register page

--- lib/features/register/presentation/view/register_page.dart
+++ lib/features/register/presentation/view/register_page.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:weather_app/core/widgets/glassmorphism.dart';
 
 class RegisterPage extends StatelessWidget {
-  const RegisterPage({ Key? key }) : super(key: key);
+  const RegisterPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("teste"),
+      backgroundColor: const Color(0xFFC2AFEC),
+      body: SafeArea(
+        top: false,
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5 ,
+                  height: MediaQuery.of(context).size.width * 0.5,
+                  child: Image(
+                    fit: BoxFit.fill,
+                    image: AssetImage("lib/assets/logo.png"),
+                  ),
+                ),
+              ),
+              GlassMorphism(child: TextField(), start: 0.3, end: 0.1,)
+            ],
+          ),
+        ),
       ),
-      
     );
   }
 }
